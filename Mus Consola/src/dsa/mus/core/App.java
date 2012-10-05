485117a2635ca062ae85c7f06b12fb3dbcfc6784
FRG <alexcucu_1992@hotmail.com>
2012-10-05 10:34:57 +0200

    Firt Commit! Yay!

---
+++ Mus Consola/src/dsa/mus/core/App.java
@@ -0,0 +1,17 @@
+package dsa.mus.core;
+
+public class App 
+{	
+    public static void main( String[] args )
+    {
+    	MusEngine musGame = new MusEngine( new ConsolePlayer[]{
+    		new ConsolePlayer("Fernando", 0),
+    		new ConsolePlayer("Irene", 1),
+    		new ConsolePlayer("Fede", 2),
+    		new ConsolePlayer("Toni", 3)
+    	});
+    	
+    	musGame.musStart();
+     	
+   }
+}
